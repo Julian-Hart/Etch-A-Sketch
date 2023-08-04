0f0f15fe46f19ec1a238b2e0fcbd71f80734417f
Julian-Hart <julianja.hart@gmail.com>
2023-08-04 10:42:42 -0400

    created function for changing size of grid with constraints. created function to be able to change div color within grid with mouseover event listener. created function for changing color to white, black, or random. created function to reset the board by setting all divs to a background color of white.

--- main.js
+++ main.js
@@ -1,9 +1,44 @@
-let board = document.querySelector(".board");
-board.style.gridTemplateColumns = "repeat(16, 1fr)";
-board.style.gridTemplateRows = "repeat(16, 1fr)";
+let color = "black";
 
-for (let i = 0; i < 256; i++) {
-  let square = document.createElement("div");
-  square.style.backgroundColor = "blue";
-  board.insertAdjacentElement("beforeend", square);
+function populateBoard(size) {
+  let board = document.querySelector(".board");
+  let squares = board.querySelectorAll("div");
+  squares.forEach((div) => div.remove());
+  board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+  for (let i = 0; i < size ** 2; i++) {
+    let square = document.createElement("div");
+    square.addEventListener("mouseover", colorSquare);
+    square.style.backgroundColor = "white";
+    board.insertAdjacentElement("beforeend", square);
+  }
+}
+
+populateBoard(16);
+
+function changeSize(input) {
+  if (input >= 2 && input <= 100) {
+    populateBoard(input);
+  } else {
+    console.log("Invalid size");
+  }
+}
+
+function colorSquare() {
+  if (color == "Random") {
+    this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+  } else {
+    this.style.backgroundColor = color;
+  }
+}
+
+function changeColor(choice) {
+  color = choice;
+}
+
+function resetBoard() {
+  let board = document.querySelector(".board");
+  let squares = board.querySelectorAll("div");
+  squares.forEach((div) => (div.style.backgroundColor = "White"));
 }
